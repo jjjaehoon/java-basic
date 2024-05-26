4f857f9826f4f0c838f30ca72e88edd40c3328ee
jjh <jjjaehoon@naver.com>
2024-05-26 22:20:27 +0900

    실전 자바 기본편 - 클래스와 데이터 1 - 프로젝트 환경 구성 - 클래스를 사용하는 이유 예시 코드

---
+++ src/class1/ClassStart2.java
@@ -0,0 +1,15 @@
+package class1;
+
+public class ClassStart2 {
+	//psvm 을 입력하면 아래 양식이 나옴
+	public static void main(String[] args) {
+		String[] studentNames = {"학생1", "학생2", "학생3"};
+		int[] studentAges = {15, 16, 17};
+		int[] studentGrades = {90, 88, 85};
+
+		for (int i = 0; i < studentNames.length; i++) {
+			System.out.println("이름: " + studentNames[i] + " 나이: " + studentAges[i] + " 점수: " + studentGrades[i]);
+		}
+
+	}
+}
---
+++ src/class1/ClassStart3.java
@@ -0,0 +1,24 @@
+package class1;
+
+public class ClassStart3 {
+	public static void main(String[] args) {
+		Student student1 = new Student();	//Student 클래스를 메모리에 등록하고 참조값을 student1 변수에 반환
+		student1.name = "학생1";
+		student1.age = 19;
+		student1.grade = 90;
+
+		Student student2 = new Student();
+		student2.name = "학생2";
+		student2.age = 16;
+		student2.grade = 80;
+
+		System.out.println("이름:" + student1.name + " 나이:" + student1.age + " 점수:" + student1.grade);
+		System.out.println("이름:" + student2.name + " 나이:" + student2.age + " 점수:" + student2.grade);
+
+		System.out.println(student1);	//객체 참조값 출력
+		System.out.println(student2);
+		/**
+		 * 설계도인 클래스를 사용해서 실제 메모리에 만들어진 실체를 객체 또는 인스턴스라고 한다.
+		 */
+	}
+}
