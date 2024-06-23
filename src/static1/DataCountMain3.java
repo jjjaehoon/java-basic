e54ca201cd1ec128eac2e8041e46ccdb39e81fca
JJH <jjjaehoon@naver.com>
2024-06-23 22:40:16 +0900

    실전 자바 기본편 - 자바 메모리 구조와 static 4 - static 변수 3 코드 추가

--- src/static1/DataCountMain3.java
+++ src/static1/DataCountMain3.java
@@ -10,5 +10,23 @@ public class DataCountMain3 {
 
         Data3 data3 = new Data3("C");
         System.out.println("C count= " + Data3.count);
+
+        /**
+         * 추가
+         * 인스턴스를 통한 접근
+         *
+         * 인스턴스가 저장된 힙영역을 갔다가 count가 있는 메서드 영역으로 다시 보낸다         *
+         * 인스턴스를 통한 접근은 추천하지 않는다 왜냐면
+         * 코드를 봤을 때 data4 인스턴스 변수인 것처럼 오해할 수가 있기 때문이다.
+         */
+        Data3 data4 = new Data3("D");
+        System.out.println(data4.count);
+
+        /**
+         * 클래스를 통한 접근
+         * 코드를 봤을 때 Data3 클래스의 변수인 것을 알 수 있기 때문에
+         * 더 명확하다.
+         */
+        System.out.println(Data3.count);
     }
 }
